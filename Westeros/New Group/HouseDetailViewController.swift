021b49db2ae7e625f0a5d57a650c25a66a13475d
Irma <irblancofdz@gmail.com>
2018-02-23 14:23:23 +0100

    Create syncModelWithView function in HouseDetailViewController.swift

--- Westeros/New Group/HouseDetailViewController.swift
+++ Westeros/New Group/HouseDetailViewController.swift
@@ -9,14 +9,40 @@
 import UIKit
 
 class HouseDetailViewController: UIViewController {
-
+    
+    // Mark: - Outlets
     @IBOutlet weak var houseNameLabel: UILabel!
     @IBOutlet weak var sigilImageView: UIImageView!
     @IBOutlet weak var wordsLabel: UILabel!
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Mark: - Properties
+    let model: House
+    
+    // Mark: - Initialization
+    init(model: House) {
+        //Primero limpias tu propio desorden
+        self.model = model
+        //Lamas a super
+        super.init(nibName: nil, bundle: Bundle(for: type(of: self)))
+    }
+    //Arreglar problema con los nil
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Mark: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        syncModelWithView()
+        
+    }
+    
+    // Mark: - Sync
+    func syncModelWithView(){
+        // Model -> View
+        houseNameLabel.text = "House \(model.name)"
+        sigilImageView.image = model.sigil.image
+        wordsLabel.text = model.words
     }
 
 }
