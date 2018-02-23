466b6386416b99434b3915dfaf9a92457604396f
Irma <irblancofdz@gmail.com>
2018-02-23 14:01:28 +0100

    Add controllers folder and HouseDetailViewController.swift and .xib

---
+++ Westeros/Controllers/HouseDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HouseDetailViewController.swift
+//  Westeros
+//
+//  Created by Irma Blanco on 23/02/2018.
+//  Copyright © 2018 Irma Blanco. All rights reserved.
+//
+
+import UIKit
+
+class HouseDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
